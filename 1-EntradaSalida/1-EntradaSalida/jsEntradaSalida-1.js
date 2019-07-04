9185ad49b26029796254a6dc713ec0d1979b22a7
riverosta <52511534+riverosta@users.noreply.github.com>
2019-07-03 22:19:01 -0300

    clase1

---
+++ 1-EntradaSalida/1-EntradaSalida/jsEntradaSalida-1.js
@@ -0,0 +1,10 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+function mostrar()
+{ var nombre; nombre = prompt("ingrese su nombre");
+
+
+
+	alert(nombre);
+
+}
+
---
+++ 1-EntradaSalida/1-EntradaSalida/jsEntradaSalida-4.js
@@ -0,0 +1,12 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
+*/
+function mostrar()
+{var nombre;
+	 nombre = prompt("ingrese un nombre");			
+	document.getElementById ("elNombre").value = nombre;
+	alert(nombre);
+
+}
+
