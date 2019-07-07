c5950c75b20d7231febb162a102cb493295fd652
riverosta <52511534+riverosta@users.noreply.github.com>
2019-07-06 22:53:22 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,15 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+var importe;
+var descuento;
+var total;
+importe = document.getElementById("importe").value;
+importe = parseInt(importe);
+descuento = (importe*25)/100;
+total = importe - descuento;
+document.getElementById("resultado").value = total;
+
+
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,15 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+var importe;
+var total;
+var aumento;
+importe = document.getElementById("sueldo").value;
+importe = parseInt(importe);
+aumento = (importe*10)/100
+total = aumento + importe;
+document.getElementById("resultado").value = total;
+
 
 	
 }
