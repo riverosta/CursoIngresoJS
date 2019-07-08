6a75c691adef7058987379ccd6a3d921ed51aee6
riverosta <52511534+riverosta@users.noreply.github.com>
2019-07-08 10:37:47 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+var niña=15;
+edad = document.getElementById("edad").value;
 
+if(edad==niña)
+{
+    alert("niña bonita");
+}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,14 @@
 function mostrar()
 {
 //tomo la edad  
-
-
+var edad;
+edad = document.getElementById("edad").value;
+if (edad>=18)
+{
+    alert("usted es mayor de edad");
+} 
+else 
+{
+alert("usted es menor de edad");
+}
 }//FIN DE LA FUNCIÓN
