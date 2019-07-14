8e12df49590b860854a94bc3393b7d4b20991fdf
riverosta <52511534+riverosta@users.noreply.github.com>
2019-07-14 11:33:32 -0300

    Add files via upload

--- jsInstruccionIF (10).js
+++ jsInstruccionIF (10).js
@@ -1,6 +1,28 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	/*Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4 */
+	var nota;
+	nota =  Math.floor(Math.random()*(11-0))+0;
+	if (nota == 9 || nota == 10)
+	{
 	
+		alert("exelente "+nota);
+	}
+	else
+	{
+		if (nota>4)
+		{
+			alert("APROBÓ "+nota);
+		}
+		else
+
+		{
+			alert("vamos, la proxima se puede "+nota);
+		}	
+	}
 
 }//FIN DE LA FUNCIÓN
