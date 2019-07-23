c3fb7247ab4249c6d3664ebb8f8cb3f837ebdaab
riverosta <52511534+riverosta@users.noreply.github.com>
2019-07-22 22:45:09 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,7 +3,24 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta='SI';
+	var numero;
+	while(respuesta=="SI")
+	{
+	numero=prompt("ingresar numeros");
+	while(isNaN(numero))
+	{
+		alert(numero+" no es un numero");
+		numero=prompt("ingrese un numero");
+	}
+	numero=parseInt(numero);
+
+	acumulador=numero+acumulador;
+	acumulador=parseInt(acumulador);
+	contador++;
+	respuesta = prompt("si quiere seguir ingresando N°s escriba SI, sino pulse cualquier tecla");
+
+	}
 
 
 document.getElementById('suma').value=acumulador;
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,11 +1,38 @@
 function mostrar()
 {
-
-	var contador=0;
+/*Al presionar el botón pedir números hasta que el usuario quiera, 
+sumar los que son positivos y multiplicar los negativos.*/
+	var numero;
 	var positivo=0;
 	var negativo=1;
-	
-	var respuesta='si';
+	var respuesta='SI';
+
+	while(respuesta=="SI")
+		{
+			numero=prompt("ingresar numeros");
+		
+			while(isNaN(numero))
+			{
+				alert(numero+" no es un numero");
+				numero=prompt("ingrese un numero");
+			}
+		
+			if(numero>0)
+			{
+			numero=parseInt(numero);
+			positivo=parseInt(positivo);	
+			positivo=numero+positivo;
+			}
+			else
+				{
+					negativo=numero*negativo;
+					
+				}
+		
+		respuesta = prompt("si quiere seguir ingresando números escriba SI, sino pulse cualquier tecla");
+
+	}
+
 
 
 document.getElementById('suma').value=positivo;
