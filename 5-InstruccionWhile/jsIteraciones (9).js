bfbc1cea407b088bb4069c2928a9e7dba03e8490
riverosta <52511534+riverosta@users.noreply.github.com>
2019-07-22 22:51:22 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,15 +2,34 @@ function mostrar()
 {
 
 	var contador=0;
-	// declarar variables
+	/* declarar variablesAl presionar el botón
+	 pedir números hasta que el usuario quiera, mostrar el número máximo y el número mínimo ingresado.*/
 	
 	var respuesta='si';
+	var numero;
+	var numero2;
+	var numeromax;
+	var numeromin;
 
 	while(respuesta!='no')
 	{
-		
+	 numero=prompt("ingrese numeros");
+	 numero2=prompt("ingrese otro numero");
+	 if (numero>numero2)
+	 {
+		numeromax=numero;
+	 }	
+	 else
+	 {
+		numeromin=numero2;
+	 }
+	 respuesta = prompt("seguir ingresando?")
 	
 	}
+	document.getElementById("maximo").value = numeromax;
+	document.getElementById("minimo").value = numeromin;
+
+
 
 
 
