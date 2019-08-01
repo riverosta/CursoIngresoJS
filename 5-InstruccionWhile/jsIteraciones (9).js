cb5305acb86008abfff60892ed42ce4f2bcee95d
riverosta <52511534+riverosta@users.noreply.github.com>
2019-07-31 21:53:00 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,36 +1,59 @@
 function mostrar()
 {
 
-	var contador=0;
 	/* declarar variablesAl presionar el botón
 	 pedir números hasta que el usuario quiera, mostrar el número máximo y el número mínimo ingresado.*/
 	
-	var respuesta='si';
-	var numero;
-	var numero2;
-	var numeromax;
-	var numeromin;
-
-	while(respuesta!='no')
-	{
-	 numero=prompt("ingrese numeros");
-	 numero2=prompt("ingrese otro numero");
-	 if (numero>numero2)
-	 {
-		numeromax=numero;
-	 }	
-	 else
-	 {
-		numeromin=numero2;
-	 }
-	 respuesta = prompt("seguir ingresando?")
+	 /* declarar variablesAl presionar el botón
+	  pedir números hasta que el usuario quiera, mostrar el número máximo y el número mínimo ingresado.*/
+	 
 	
-	}
-	document.getElementById("maximo").value = numeromax;
-	document.getElementById("minimo").value = numeromin;
-
-
-
+	  var respuesta;
+	  var numero;
+	  var maximo;
+	  var minimo;
+	  var esPrimeraIteracion = true;
+  
+	  
+	  // declarar variables
+  
+	
+	  do
+	  {
+  
+  
+	  
+  
+		  numero = prompt("Ingrese un numero");
+		  numero = parseInt(numero);
+  
+		  while(isNaN(numero) || numero < -10
+		   || numero > 10) {
+			  numero = prompt("Error. Debe ingresar un numero entre -10 y 10. Reingrese:");			
+			  numero = parseInt(numero);
+		  }
+  
+  
+		  if(esPrimeraIteracion === true)
+		  {
+			  maximo = numero;
+			  minimo = numero;
+			  esPrimeraIteracion = false;
+		  }
+		  else if(numero > maximo) 
+		  {
+			  maximo = numero;
+		  }
+		  else if(numero < minimo)
+		  {
+			  minimo = numero;
+		  }
+  
+		   respuesta = confirm("¿Desea continuar?");
+	  } while(respuesta);
+  
+	  document.write("Máximo = " + maximo + "<br>");
+	  document.write("Mínimo = " + minimo + "<br>");
 
 
 
