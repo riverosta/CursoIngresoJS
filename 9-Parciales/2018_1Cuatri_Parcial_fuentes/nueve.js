689274ab50da1dd1e8e62cfcd6ce627e6753623d
riverosta <52511534+riverosta@users.noreply.github.com>
2019-08-03 23:40:13 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,83 @@
 function mostrar()
 {
+/*Bienvenidos. 
+Realizar el algoritmo que permita ingresar la marca del producto, el peso el cual debe ser entre 1 y 100 (validar), 
+la temperatura de almacenamiento(entre -30 y 30) hasta que el usuario quiera e informar al terminar el ingreso por document.write: 
+a) La cantidad de temperaturas pares. 
+b) La marca del producto más pesado 
+c) La cantidad de productos que se conservan a menos de 0 grados. 
+d) El promedio del peso de todos los productos.	
+f) El peso máximo y el mínimo. */
+
+var marca;
+var peso;
+var temperatura;
+var respuesta="si";
+var contador=0;
+var par=0;
+var primeraMarca=0;
+var masPesado=0;
+var otraMarca;
+var menosPesado;
+
+while (respuesta =="si")
+{
+    marca=prompt("ingrese marca");
+    while(!isNaN(marca) || marca=="pepito")
+    {
+        marca=prompt("marca incorrecta, vuelva a ingresar");
+    }
+
+    peso=prompt("ingrese peso");
+    peso=parseInt(peso);
+
+    while(isNaN(peso) || peso<1 || peso>100)
+    {
+        peso=prompt("peso incorrecto, vuelva a ingresar")
+        peso=parseInt(peso);
+    }
+
+    temperatura=prompt("ingrese temperatura");
+    temperatura=parseInt(temperatura);
+    while(isNaN(temperatura)|| temperatura>30 || temperatura<-30 )
+    {
+        temperatura=prompt("error, vuelva a ingresar temperatura");
+        temperatura=parseInt(temperatura);
+    }
+    if(temperatura%2==0)
+    {
+        par++
+    }
+
+    if (contador==0)
+    {
+        primeraMarca=marca;
+        masPesado=peso;
+    }
+    if(masPesado<peso)
+    {
+        masPesado=peso
+        primeraMarca=marca;
+    }
+    else 
+    {
+        menosPesado=peso;
+        otraMarca = marca
+    }
+
+
+
+
+
+
+
+
+    contador++;
+    respuesta=prompt("¿desea seguir ingresando?")
+}
+
+document.write("las temperaturas par ingresados fueron:"+par+"<br>");
+document.write("el mas pesado fue " +masPesado+ " y la marca "+primeraMarca+"<br>");
+document.write("el menos pesado " +menosPesado+ " y la marca " +otraMarca+"<br>");
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,59 +1,49 @@
 function mostrar()
 {
-    /*Realizar el algoritmo que permita el ingreso por prompt de las notas
-    (validar entre 0 y 10) , el sexo (validar el sexo “f” o “m”) de 5 alumnos, informar por alert: 
-    a) El promedio de las notas totales. 
-    b) La nota más baja y el sexo de esa persona. 
-    c) La cantidad de varones que su nota haya sido mayor o igual a 6.*/
-    var contador=0;
-    var nota=0;
-    var sexo ;
-    var promedio;
-    var notatotal=0;
-    var notabaja=0;
-    var sexomostrar;
-    while(contador<5)
+/*Realizar el algoritmo que permita el ingreso por prompt de las notas
+ (validar entre 0 y 10) , el sexo (validar el sexo “f” o “m”) de 5 alumnos, informar por alert: 
+a) El promedio de las notas totales. 
+b) La nota más baja y el sexo de esa persona. 
+c) La cantidad de varones que su nota haya sido mayor o igual a 6.*/
+var contador=0;
+var nota;
+var sexo ;
+var sexof=1;
+var sexom=1;
+var promedio;
+var notatotal=0;
+while(contador<5)
+{
+    nota =prompt("ingresar nota");
+    while ((isNaN(nota) || (nota>10 || nota<0)))
     {
-        nota =prompt("ingresar nota");
-        while ((isNaN(nota) || (nota>10 || nota<0)))
-        {
-            alert(nota+"no es una nota valida");
-            nota = prompt("ingreasar nota");
-        }
-        nota=parseInt(nota);
+        alert(nota+"no es una nota valida");
+        nota = prompt("ingreasar nota");
 
-        notatotal=nota+notatotal;
-
-        sexo=prompt("ingrese su sexo f o m")
-        while(sexo!="f" && sexo!="m")
-        {
-            alert(sexo+" no es válido");
-            sexo=prompt("ingrese sexo");
-        }
-
-        if(contador==0)
-        {
-            notabaja=nota;
-            sexomostrar=sexo;
-        }
-        else if(nota<notabaja)
-        {
-            notabaja = nota;
-            sexomostar = sexo;
-        }  
-
-        if ( nota>=6 && sexo=="m")
-        {
-            varones++;
-        }
-
-
-
-
-        contador++;
     }
-    alert("la nota mas baja fue :"+notabaja+"y su sexo es: "+sexomostrar);
-    alert("el promedio es: "+(notatotal/5));
-    alert("la cantidad de varones que obtuvieron nota baja fueron: "+varones);
+    nota=parseInt(nota);
+ notatotal=nota+notatotal;
+       
+   sexo=prompt("ingrese su sexo f o m")
+   while(sexo!="f" && sexo!="m")
+   {
+    alert(sexo+" no es válido");
+    sexo=prompt("ingrese sexo")   
+   }
 
+   if (sexo=="f")
+   {
+       sexof++
+   }
+    else  if (sexo=="m")
+    {
+        sexom++
+    }
+   
+   
+contador++;
+}
+alert("el promedio es: "+(notatotal/5));
+alert(sexof+" sexo femenino");
+alert(sexom+" sexo masculino");
 }
