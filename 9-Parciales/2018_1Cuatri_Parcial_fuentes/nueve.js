6db09c3af71db0bbd1e97c8ab1210f5e23d9ca86
riverosta <52511534+riverosta@users.noreply.github.com>
2019-08-11 12:33:07 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -8,85 +8,95 @@ b) La marca del producto más pesado
 c) La cantidad de productos que se conservan a menos de 0 grados. 
 d) El promedio del peso de todos los productos.	
 f) El peso máximo y el mínimo. */
-
 var marca;
 var peso;
 var temperatura;
-var contador=0;
-var par=0;
-var primeraMarca=0;
-var masPesado=0;
-var otraMarca;
-var menosPesado;
-var contadorProducto=0;
-var pesoTotal=0;
-var contadorTemperatura=0;
 var respuesta;
+var contador=0;
+var contadorPares=0;
+var masPesado;
+var marcaMasPesado;
+var contadorTemperatura=0;
+var pesoTotal=0;
+var menosPesado;
+var marcaMenosPesado;
 
 do
 {
-    marca=prompt("ingrese marca");
-    while(!isNaN(marca) || marca=="pepito")
-    {
-        marca=prompt("marca incorrecta, vuelva a ingresar");
-    }
+marca=prompt("ingresar marca");
+peso=prompt("ingresar peso");
+peso=parseInt(peso);
 
-    peso=prompt("ingrese peso");
-    peso=parseInt(peso);
-
-    while(isNaN(peso) || peso<1 || peso>100)
-    {
-        peso=prompt("peso incorrecto, vuelva a ingresar")
-        peso=parseInt(peso);
-    }
-    pesoTotal=peso+pesoTotal;
-
-    temperatura=prompt("ingrese temperatura");
+while(isNaN(peso) || peso>100 || peso<1)
+{
+    peso=prompt("error vuelva a ingresar");
+    peso= parseInt(peso);
+}
+temperatura=prompt("ingrese temperatura");
+temperatura=parseInt(temperatura);
+while(isNaN(temperatura) || temperatura>30 || temperatura<-30)
+{
+    temperatura=prompt("error, vuelva a ingresar");
     temperatura=parseInt(temperatura);
-    while(isNaN(temperatura)|| temperatura>30 || temperatura<-30 )
-    {
-        temperatura=prompt("error, vuelva a ingresar temperatura");
-        temperatura=parseInt(temperatura);
-    }
-    if(temperatura%2==0)
-    {
-        par++
-    }
+}
 
-    if (contador==0)
-    {
-        primeraMarca=marca;
-        masPesado=peso;
-    }
-    if(masPesado<peso)
-    {
-        masPesado=peso
-        primeraMarca=marca;
-    }
-    else 
-    {
-        menosPesado=peso;
-        otraMarca = marca
-    }
-    if (temperatura<0)
-    {
-        contadorTemperatura++;
-    }
+if(temperatura%2==0)
+{
+    contadorPares++;
+}
+if(contador==0)
+{
+    MasPesado=peso;
+    marcaMasPesado=marca;
+    menosPesado=peso;
+    marcaMenosPesado=marca;
+
+}
+if(masPesado<peso)
+{
+    masPesado=peso
+    marcaMasPesado=marca;
+}
+else if(menosPesado<peso)
+{
+    menosPesado=peso;
+    marcaMenosPesado=peso;
+}
 
 
 
+if(temperatura<0)
+{
+    contadorTemperatura++;
+}
+pesoTotal=peso+pesoTotal;
 
 
 
 
-    contador++;
-    respuesta=confirm("desea seguir ingresando?");
+
+
+
+
+contador++;
+respuesta=confirm("¿desea contuniar?");
+
 }while(respuesta);
 
 
-document.write("las temperaturas par ingresados fueron:"+par+"<br>");
-document.write("la marca más pesado: "+primeraMarca+"<br>");
-document.write("el peso mínimo fue " +menosPesado+ " y la marca " +otraMarca+ " y el maximo fue " +masPesado+" y la marca "+primeraMarca+ " <br>");
-document.write("la cantidad de temperatura que se conserva a menos de 0 grados es: "+contadorTemperatura+"<br>");
-document.write("el promedio del peso total de los podrucctos ingresasos fueron :"+(pesoTotal/contador)+"<br>");
+
+
+
+document.write("la cantidad de temperaturas pares es: "+contadorPares+"<br>");
+document.write("la marca del producto mas pesado es: "+marcaMasPesado+"<br>");
+document.write("la cantidad e temperatura que se conserva a nemos de 0 grados son: "+contadorTemperatura+"<br>");
+document.write("el promedio del peso de los productos es: "+pesoTotal/contador+"<br>");
+document.write("el peso maximo es "+masPesado+"<br>");
+document.write("el peso minimo es "+menosPesado+"<br>");
+
+
+
+
+
+
 }
